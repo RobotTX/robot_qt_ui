9d14116428f2c69e83a9bc1c06143312d1a10cee
M-A <marc.testier@gtdollar.com>
2016-06-23 15:11:21 +0800

    Added some comments

--- View/spacewidget.h
+++ View/spacewidget.h
@@ -4,10 +4,20 @@
 #include <QWidget>
 class QFrame;
 
+/**
+ * @brief The SpaceWidget class
+ * Draw a vertical or horizontal line
+ */
 class SpaceWidget : public QWidget{
 public:
     enum SpaceOrientation {VERTICAL, HORIZONTAL};
     SpaceWidget(SpaceOrientation orientation);
+
+    /**
+     * @brief setColor
+     * @param color
+     * To change the color of the line, can use any HTML color recognized by the stylesheet
+     */
     void setColor(QString color);
 
 private:
