b94ea468387cf839e379ca6fa02010890b57ba48
Joan <joan.coquelin@gtdollar.com>
2016-11-10 16:58:29 +0800

    settings work on windows as well

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 #include "Controller/mainwindow.h"
 #include <QStyle>
 #include <QDesktopWidget>
+#include <QSettings>
+#include <QDir>
 
 int main(int argc, char *argv[]){
 
     QApplication app(argc, argv);
     app.setAttribute(Qt::AA_DontShowIconsInMenus, false);
 
-    MainWindow w;
+    QSettings settings("GTDollar", "gobot-software");
+    settings.setPath(QSettings::IniFormat, QSettings::UserScope, QDir::currentPath());
+
+    MainWindow w(&settings);
     w.setMinimumSize(1000, 800);
     /// to center the application on the screen
     w.setGeometry(
